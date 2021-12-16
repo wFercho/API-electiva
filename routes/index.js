f6710f98d822afe8d847f9f67ee14f7e7903ae26
juan-Baracaldo-rojas <87029593+juan-Baracaldo-rojas@users.noreply.github.com>
2021-12-16 17:20:22 -0500

    actualizacion rutas Detail
    se anexo
    const {indexDetail,getDetail,newDetail,updateDetail,deleteDetail} = require('../controllers/detail')
    router.get('/getDetails',indexDetail)
    router.post('/newDetail',newDetail)
    router.get('/getDetail/:cant', getDetail)
    router.get('/updateDetail/:cant', updateDetail)
    router.get('/deleteDetail/:cant', deleteDetail)

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,19 @@
 'use strict'
 
 const {index,getProduct,updateProduct,deleteProduct} = require('../controllers/product')
-
+const {indexDetail,getDetail,newDetail,updateDetail,deleteDetail} = require('../controllers/detail')
 const productController = require('../controllers/product')
 const {indexBill,newBill,getBill,updateBill,deleteBill} = require('../controllers/bill')
 const express = require('express')
 const { model } = require('mongoose')
 const router = express.Router()
+
+// router.get('/getDetails',detailController.getDetails)
+router.get('/getDetails',indexDetail)
+router.post('/newDetail',newDetail)
+router.get('/getDetail/:cant', getDetail)
+router.get('/updateDetail/:cant', updateDetail)
+router.get('/deleteDetail/:cant', deleteDetail)
 
 //router.get('/getbills', billController.getBills)
 router.get('/getbills',indexBill)
@@ -28,3 +35,4 @@ router.delete('/deleteproduct/:idProduct', deleteProduct)
 router.put('/updateproduct/:idProduct',updateProduct)
  
 module.exports = router;
+
